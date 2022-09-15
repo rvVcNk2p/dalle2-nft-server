48a16937cddd018542ae3a896b806e45ac050d4c
Kussy Leo <kussynorbertleo@gmail.com>
2022-09-15 19:50:02 +0200

    Dalle-2 integrated

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const express = require('express')
+import express from 'express';
+import { Dalle } from 'dalle-node';
+import * as dotenv from 'dotenv'
 const app = express()
 
-require('dotenv').config()
+dotenv.config()
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!'})
+app.get('/fetch-images', async (req, res) => {
+  try {
+    const dalle = new Dalle(process.env.BEARER_TOKEN);
+    const creditsSummary = await dalle.getCredits();
+    const totalCreditsLeft = creditsSummary.aggregate_credits;
+    
+    res.json({ message: 'Hello World!', totalCreditsLeft})
+  } catch(err) {
+    res.json({ errorMessage: 'Something went wrong!'})
+  }
 })
 
 app.listen(port, () => {
