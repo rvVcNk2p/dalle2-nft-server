2da35a5995d4bc221168782a8c8c659cca5b7c92
Kussy Leo <kussynorbertleo@gmail.com>
2022-09-16 00:44:49 +0200

    feat(app): Invalid images removed

--- dummyData/testGenerations.js
+++ dummyData/testGenerations.js
@@ -1,69 +1,67 @@
 export const dummyGenerations = () => {
-    const randomNum = Math.floor(Math.random() * 3) // Random number between 0 and 2
+    const randomNum = Math.floor(Math.random() * 2) // Random number between 0 and 1
     switch(randomNum) {
         case 0:
         return dummyData_0
         case 1:
         return dummyData_1
-        case 2:
-        return dummyData_2
     }
 };
 
-const dummyData_0 = {
-    data: [
-        {
-            id: "generation-RrcnKhmRjZCvcnuScEba8nUZ",
-            object: "generation",
-            created: 1663277464,
-            generation_type: "ImageGeneration",
-            generation: {
-                image_path: "https://openailabsprodscus.blob.core.windows.net/private/user-ZAoknTNRK9ta6uhO6P707bfQ/generations/generation-RrcnKhmRjZCvcnuScEba8nUZ/image.webp?st=2022-09-15T20%3A32%3A06Z&se=2022-09-15T22%3A30%3A06Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-09-15T20%3A43%3A08Z&ske=2022-09-22T20%3A43%3A08Z&sks=b&skv=2021-08-06&sig=vIL5zEbWjh2H/b34mRNo6YDvQzaCDVoCnf/5F7Nx7ZY%3D"
-            },
-            task_id: "task-elAZ3GpdCLy8WuzUrWHFPetg",
-            prompt_id: "prompt-qperwaKATjvsFzwLDJCsXmwk",
-            is_public: false
-        },
-        {
-            id: "generation-JdkehXo6RiAO4XybZ5q7LYb1",
-            object: "generation",
-            created: 1663277463,
-            generation_type: "ImageGeneration",
-            generation: {
-                image_path: "https://openailabsprodscus.blob.core.windows.net/private/user-ZAoknTNRK9ta6uhO6P707bfQ/generations/generation-JdkehXo6RiAO4XybZ5q7LYb1/image.webp?st=2022-09-15T20%3A32%3A06Z&se=2022-09-15T22%3A30%3A06Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-09-15T20%3A43%3A08Z&ske=2022-09-22T20%3A43%3A08Z&sks=b&skv=2021-08-06&sig=Uv0VfGTE8S7FB%2BDSRqvUcS8ZlsHUli%2BHzEiuJpzoag0%3D"
-            },
-            task_id: "task-elAZ3GpdCLy8WuzUrWHFPetg",
-            prompt_id: "prompt-qperwaKATjvsFzwLDJCsXmwk",
-            is_public: false
-        },
-        {
-            id: "generation-ezNO9yMOuAsIo61nDZZ9s9wT",
-            object: "generation",
-            created: 1663277463,
-            generation_type: "ImageGeneration",
-            generation: {
-                image_path: "https://openailabsprodscus.blob.core.windows.net/private/user-ZAoknTNRK9ta6uhO6P707bfQ/generations/generation-ezNO9yMOuAsIo61nDZZ9s9wT/image.webp?st=2022-09-15T20%3A32%3A06Z&se=2022-09-15T22%3A30%3A06Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-09-15T20%3A43%3A08Z&ske=2022-09-22T20%3A43%3A08Z&sks=b&skv=2021-08-06&sig=WnHMWhfr%2BPno6578sN1RQNSZFdIpcUXiVOr9tN6eNbE%3D"
-            },
-            task_id: "task-elAZ3GpdCLy8WuzUrWHFPetg",
-            prompt_id: "prompt-qperwaKATjvsFzwLDJCsXmwk",
-            is_public: false
-        },
-        {
-            id: "generation-vYoDdVyjfWDrIndjApN5qIQY",
-            object: "generation",
-            created: 1663277464,
-            generation_type: "ImageGeneration",
-            generation: {
-                image_path: "https://openailabsprodscus.blob.core.windows.net/private/user-ZAoknTNRK9ta6uhO6P707bfQ/generations/generation-vYoDdVyjfWDrIndjApN5qIQY/image.webp?st=2022-09-15T20%3A32%3A06Z&se=2022-09-15T22%3A30%3A06Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-09-15T20%3A43%3A08Z&ske=2022-09-22T20%3A43%3A08Z&sks=b&skv=2021-08-06&sig=NzHzxgp9LT5x53mawmEdjSzkIFOezd7HshBxtwbeyIo%3D"
-            },
-            task_id: "task-elAZ3GpdCLy8WuzUrWHFPetg",
-            prompt_id: "prompt-qperwaKATjvsFzwLDJCsXmwk",
-            is_public: false
-        }
-    ]
-}
+// const dummyData_0 = {
+//     data: [
+//         {
+//             id: "generation-RrcnKhmRjZCvcnuScEba8nUZ",
+//             object: "generation",
+//             created: 1663277464,
+//             generation_type: "ImageGeneration",
+//             generation: {
+//                 image_path: "https://openailabsprodscus.blob.core.windows.net/private/user-ZAoknTNRK9ta6uhO6P707bfQ/generations/generation-RrcnKhmRjZCvcnuScEba8nUZ/image.webp?st=2022-09-15T20%3A32%3A06Z&se=2022-09-15T22%3A30%3A06Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-09-15T20%3A43%3A08Z&ske=2022-09-22T20%3A43%3A08Z&sks=b&skv=2021-08-06&sig=vIL5zEbWjh2H/b34mRNo6YDvQzaCDVoCnf/5F7Nx7ZY%3D"
+//             },
+//             task_id: "task-elAZ3GpdCLy8WuzUrWHFPetg",
+//             prompt_id: "prompt-qperwaKATjvsFzwLDJCsXmwk",
+//             is_public: false
+//         },
+//         {
+//             id: "generation-JdkehXo6RiAO4XybZ5q7LYb1",
+//             object: "generation",
+//             created: 1663277463,
+//             generation_type: "ImageGeneration",
+//             generation: {
+//                 image_path: "https://openailabsprodscus.blob.core.windows.net/private/user-ZAoknTNRK9ta6uhO6P707bfQ/generations/generation-JdkehXo6RiAO4XybZ5q7LYb1/image.webp?st=2022-09-15T20%3A32%3A06Z&se=2022-09-15T22%3A30%3A06Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-09-15T20%3A43%3A08Z&ske=2022-09-22T20%3A43%3A08Z&sks=b&skv=2021-08-06&sig=Uv0VfGTE8S7FB%2BDSRqvUcS8ZlsHUli%2BHzEiuJpzoag0%3D"
+//             },
+//             task_id: "task-elAZ3GpdCLy8WuzUrWHFPetg",
+//             prompt_id: "prompt-qperwaKATjvsFzwLDJCsXmwk",
+//             is_public: false
+//         },
+//         {
+//             id: "generation-ezNO9yMOuAsIo61nDZZ9s9wT",
+//             object: "generation",
+//             created: 1663277463,
+//             generation_type: "ImageGeneration",
+//             generation: {
+//                 image_path: "https://openailabsprodscus.blob.core.windows.net/private/user-ZAoknTNRK9ta6uhO6P707bfQ/generations/generation-ezNO9yMOuAsIo61nDZZ9s9wT/image.webp?st=2022-09-15T20%3A32%3A06Z&se=2022-09-15T22%3A30%3A06Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-09-15T20%3A43%3A08Z&ske=2022-09-22T20%3A43%3A08Z&sks=b&skv=2021-08-06&sig=WnHMWhfr%2BPno6578sN1RQNSZFdIpcUXiVOr9tN6eNbE%3D"
+//             },
+//             task_id: "task-elAZ3GpdCLy8WuzUrWHFPetg",
+//             prompt_id: "prompt-qperwaKATjvsFzwLDJCsXmwk",
+//             is_public: false
+//         },
+//         {
+//             id: "generation-vYoDdVyjfWDrIndjApN5qIQY",
+//             object: "generation",
+//             created: 1663277464,
+//             generation_type: "ImageGeneration",
+//             generation: {
+//                 image_path: "https://openailabsprodscus.blob.core.windows.net/private/user-ZAoknTNRK9ta6uhO6P707bfQ/generations/generation-vYoDdVyjfWDrIndjApN5qIQY/image.webp?st=2022-09-15T20%3A32%3A06Z&se=2022-09-15T22%3A30%3A06Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-09-15T20%3A43%3A08Z&ske=2022-09-22T20%3A43%3A08Z&sks=b&skv=2021-08-06&sig=NzHzxgp9LT5x53mawmEdjSzkIFOezd7HshBxtwbeyIo%3D"
+//             },
+//             task_id: "task-elAZ3GpdCLy8WuzUrWHFPetg",
+//             prompt_id: "prompt-qperwaKATjvsFzwLDJCsXmwk",
+//             is_public: false
+//         }
+//     ]
+// }
 
-const dummyData_1 = {
+const dummyData_0 = {
     data: [
         {
             id: "generation-qM0FkuhBKrezdyMZO6AaXyP7",
@@ -116,7 +114,7 @@ const dummyData_1 = {
     ]
 }
 
-const dummyData_2 = {
+const dummyData_1 = {
     data: [
         {
             id: "generation-UklyjMdHZ3hlMBgM77u5wk8n",
