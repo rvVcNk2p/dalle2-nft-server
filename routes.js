ea694942b3839f0e407e90fac7c91f172fc0063b
Kussy Leo <kussynorbertleo@gmail.com>
2022-09-22 14:08:50 +0200

    feat(app): Error handling updated

--- routes.js
+++ routes.js
@@ -47,12 +47,15 @@ const initRouter = (app) => {
 
 	app.post('/set-nft', async (req, res) => {
 		const { tokenId, imgId, imagePath, taskId, address } = req.body
+
 		const { isOwner, error: ownershipErr } = await checkNftOwnership(
 			address,
 			tokenId,
 		)
 		// If the request initiator do not owns the NFT throw an error
 		if (!isOwner || ownershipErr) {
+			const { code, reason } = ownershipErr
+			console.log(`❌ [${code}] Nft status error: ${reason}`)
 			res.json({
 				field: 'owner',
 				value: isOwner,
@@ -66,10 +69,12 @@ const initRouter = (app) => {
 		const { isAlreadyUpdated, error: statusErr } = await checkNftStatus(tokenId)
 		// If the requested NFT has already been setted
 		if (isAlreadyUpdated || statusErr) {
+			const { code, reason } = statusErr
+			console.log(`❌ [${code}] Nft status error: ${reason}`)
 			res.json({
 				field: 'status',
 				value: isAlreadyUpdated,
-				error: ownershipErr,
+				error: statusErr,
 			})
 		}
 		if (!isAlreadyUpdated) {
@@ -79,10 +84,12 @@ const initRouter = (app) => {
 		const { cid, error: ipfsErr } = await uploadImgToIpfs(imagePath)
 		// If the uploadable image ipfs upload has been failed
 		if (cid === null || ipfsErr) {
+			const { code, reason } = ipfsErr
+			console.log(`❌ [${code}] IPFS error: ${reason}`)
 			res.json({
 				field: 'ipfs',
 				value: cid,
-				error: ownershipErr,
+				error: ipfsErr,
 			})
 		}
 		if (cid) {
@@ -101,17 +108,20 @@ const initRouter = (app) => {
 			maxFeePerGas,
 			maxPriorityFeePerGas,
 		)
-		if (txResponse === 'ERROR') {
-			res.json({
-				field: 'txResponse',
-				value: 'ERROR',
-				error: txError,
-			})
-		} else {
+
+		if (txResponse === 'SUCCESS') {
 			console.log(`✅ The requested image has been setted!`)
 			res.json({
 				field: 'txResponse',
 				value: 'SUCCESS',
+				error: txError,
+			})
+		} else {
+			const { code, reason } = txError
+			console.log(`❌ [${code}] txResponse: ${reason}`)
+			res.json({
+				field: 'txResponse',
+				value: 'ERROR',
 				error: txError,
 			})
 		}
